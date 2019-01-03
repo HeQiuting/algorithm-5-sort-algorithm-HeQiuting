35a0d04c3a78c4d515d4a63712c59b1605a646f7
HeQiuting <45375773+HeQiuting@users.noreply.github.com>
2019-01-03 15:01:42 +0800

    Update SortAlgorithm.java

--- SortAlgorithm.java
+++ SortAlgorithm.java
@@ -1,3 +1,61 @@
 public class SortAlgorithm{
+    public static void mergeSort(int[] ints, int leftStart, int rightEnd){
+        // base case
+        if(leftStart == rightEnd){
+            return;
+        }
+        // sort left half
+        int leftEnd = (rightEnd - leftStart)/2 + leftStart;
+        mergeSort(ints, leftStart, leftEnd);
+        mergeSort(ints, leftEnd + 1, rightEnd);
+        merge(ints, leftStart, leftEnd, rightEnd);
+    }
+    public static void merge(int[] ints, int leftStart, int leftEnd, int rightEnd){
+        int[] tmp = new int[rightEnd - leftStart + 1];
+        int l = leftStart;
+        int r = leftEnd + 1;
+        int index = 0;
+        while(l <= leftEnd && r <= rightEnd){
+            if(ints[l] < ints[r]){
+                tmp[index] = ints[l];
+                index++;
+                l++;
+            }
+            else if(ints[r] < ints[l]){
+                tmp[index] = ints[r];
+                index++;
+                r++;
+            }
+            else{
+                tmp[index] = ints[l];
+                index++;
+                l++;
+            }
+        }
+        while(l <= leftEnd){
+            tmp[index] = ints[l];
+            index++;
+            l++;
+        }
+        while(r <= rightEnd){
+            tmp[index] = ints[r];
+            index++;
+            r++;
+        }
+        for(int i = 0; i < tmp.length; i++){
+            ints[leftStart + i] = tmp[i];
+        }
+    }
+    public static void print(int[] ints){
+        for(int e : ints){
+            System.out.print(e + " ");
+        }
+    }
 
+    public static void main(String[] args){
+        int[] ints = {1, 3, -1, 5};
+        //            0  1   2  3
+        merge(ints, 0, 1, 3);
+        print(ints);
+    }
 }
